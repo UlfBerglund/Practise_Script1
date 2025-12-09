4f1286032802411226c6a46bfa9c1de2bc886591
Ulf <ulf.berglund84@gmail.com>
2025-12-09 07:57:47 -0500

    Min första commit, lade till readme fil

---
+++ övning1a.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Detta script samlar in systeminformation - RECON
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Ulf Berglund
+# Last Update: 2025-01-01
+
+
+echo "Välkommen till RECON script för att kontrollera en Linux-miljö"
+
+echo
+echo "=== SYSTEMINFO ==="
+uname -a
+
+echo
+echo "=== AKTUELL ANVÄNDARE  ==="
+echo $USER
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+
+echo
+echo "=== LÄGG TILL FLERA TESTER  ==="
+
+echo "=== HÅRDVARA  ==="
+lscpu
+echo "== HUR MYCKET RAM ÄR LEDIGT/ANVÄNDS? =="
+free -h
+echo "== ADMIN =="
+getent group sudo
+echo "== SENASTE INLOGGNINGAR =="
+last -a | head
+echo "== ROUTING =="
+ip route
+echo "== AKTIVA PORTAR  =="
+ss -tuln
+echo "== DATUM OCH TID =="
+date
+
